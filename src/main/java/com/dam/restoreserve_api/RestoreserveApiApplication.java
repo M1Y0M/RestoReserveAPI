0637825c882849bca89af099da136c31ec38037d
Javier <javierprt08gmail.com>
2026-05-06 09:51:51 +0100

    Primer commit

---
+++ src/main/java/com/dam/restoreserve_api/RestoreserveApiApplication.java
@@ -0,0 +1,13 @@
+package com.dam.restoreserve_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoreserveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoreserveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/restoreserve_api/RestoreserveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.restoreserve_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoreserveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
